7cf8916bae14733d14c88f58d7063d074f44ee19
Khanshot <170107038@stu.sdu.edu.kz>
2023-03-09 16:15:02 +0600

    as

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'unique:users','digits:11'],
+            'phone' => ['required', 'unique:users','digits:12'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
